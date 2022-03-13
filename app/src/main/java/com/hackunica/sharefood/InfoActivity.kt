f6e70ba0246b2a585caf28ad08e7ac831bd8d698
ferchi <fsalinas628@gmail.com>
2022-03-13 16:31:25 -0600

    Implementacion de Info de post

--- app/src/main/java/com/hackunica/sharefood/InfoActivity.kt
+++ app/src/main/java/com/hackunica/sharefood/InfoActivity.kt
@@ -1,11 +1,67 @@
 package com.hackunica.sharefood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.hackunica.sharefood.databinding.ActivityCreateBinding
+import com.hackunica.sharefood.databinding.ActivityInfoBinding
 
 class InfoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityInfoBinding
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+        database = Firebase.database.reference
+
+        binding = ActivityInfoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val intent = intent;
+        val address = intent.getStringExtra("address")
+        val info = intent.getStringExtra("info")
+        val status = intent.getStringExtra("status")
+        val date = intent.getStringExtra("date")
+        val postId = intent.getStringExtra("id")
+
+        binding.tvInfoAddress.text = address
+        binding.tvInfo.text = info
+        binding.status.text = status
+        binding.tvDate.text = date
+
+        loadImg(binding.ivInfo, postId!!)
     }
+
+    private fun loadImg(view: ImageView, postId:String) {
+
+        database.child("Donations").child(postId).child("image").get().addOnSuccessListener {
+            val urlImg = it.value.toString()
+            print("UrlImg: $urlImg")
+
+            try {
+                Glide.with(this)
+                    .load(urlImg)
+                    .transform(CenterCrop())
+                    .into(view)
+
+            } catch (e: Exception) {
+
+            }
+        }.addOnFailureListener{
+            Log.e("firebase", "Error getting data", it)
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/hackunica/sharefood/models/DonationAdapter.kt
+++ app/src/main/java/com/hackunica/sharefood/models/DonationAdapter.kt
@@ -8,12 +8,18 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.animation.animpresseffect.PressEffectCardView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.hackunica.sharefood.R
+
+import android.content.Intent
+import androidx.core.content.ContextCompat.startActivity
+import com.hackunica.sharefood.InfoActivity
+
 
 class DonationAdapter(private val donationList : ArrayList<Donacion>) : RecyclerView.Adapter <DonationAdapter.DonationViewHolder>(){
     lateinit var postId : String
@@ -38,6 +44,16 @@ class DonationAdapter(private val donationList : ArrayList<Donacion>) : Recycler
         postId = currentItem.id.toString()
 
         loadImg(holder.image)
+
+        holder.cardInfo.setOnClickListener {
+            val intent = Intent(context, InfoActivity::class.java)
+            intent.putExtra("address", currentItem.address)
+            intent.putExtra("info", currentItem.info)
+            intent.putExtra("status", currentItem.status)
+            intent.putExtra("date", currentItem.date)
+            intent.putExtra("id", postId)
+            context.startActivity(intent);
+        }
     }
 
     override fun getItemCount(): Int {
@@ -49,6 +65,7 @@ class DonationAdapter(private val donationList : ArrayList<Donacion>) : Recycler
         val info : TextView =  itemView.findViewById(R.id.tv_info)
         val status : TextView = itemView.findViewById(R.id.tv_status)
         val date : TextView = itemView.findViewById(R.id.tv_date)
+        val cardInfo: PressEffectCardView = itemView.findViewById(R.id.card_view_item)
     }
 
     private fun loadImg(view: ImageView) {
