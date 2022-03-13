beecab4941000b5867cac3e8ca821eec1799f2ed
ferchi <fsalinas628@gmail.com>
2022-03-12 22:11:25 -0600

    Implementacion de navegacion

--- app/src/main/java/com/hackunica/sharefood/LoginActivity.kt
+++ app/src/main/java/com/hackunica/sharefood/LoginActivity.kt
@@ -2,10 +2,26 @@ package com.hackunica.sharefood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hackunica.sharefood.databinding.ActivityLoginBinding
+import com.hackunica.sharefood.databinding.ActivityMainBinding
+import android.content.Intent
+
+
+
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/hackunica/sharefood/MainActivity.kt
+++ app/src/main/java/com/hackunica/sharefood/MainActivity.kt
@@ -2,10 +2,32 @@ package com.hackunica.sharefood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.hackunica.sharefood.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        navController = findNavController(R.id.home_fragment)
+        setupSmoothBottomMenu()
+    }
+
+
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.main_menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
     }
 }
